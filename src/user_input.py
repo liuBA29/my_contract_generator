f4dbe0df42c859778c21fa16eebf72bb7676c09c
lu- <lu->
2025-05-15 14:13:02 +0300

    Ðpush

---
+++ src/user_input.py
@@ -0,0 +1,24 @@
+from src.database import *
+
+def get_user_input():
+    contract_number = input("Введите номер договора: ")
+    location = input("Введите место проведения работ: ")
+    doc_date = input("Введите дату (дд.мм.гггг): ")
+    work_list = []
+    while True:
+        work = input("Введите  работы: ")
+        if work == "":
+            break
+        work_list.append(work)
+
+        # Ввод стоимости
+    while True:
+        try:
+            total_cost = float(input("Введите стоимость работ (в рублях): "))
+            break
+        except ValueError:
+            print("Пожалуйста, введите числовое значение.")
+
+    return contract_number, location, doc_date, work_list, total_cost
+
+
